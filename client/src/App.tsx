4002a0d9b2b097a7e19584f84c69379e0fa2ba41
loqman1235 <tonyvito12@gmail.com>
2024-06-27 11:27:50 +0100

    Chore: Lazy load AppLayout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,31 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-import AppLayout from "@/layouts/AppLayout";
+// import AppLayout from "@/layouts/AppLayout";
 import NotesPage from "@/pages/NotesPage";
 import RemindersPage from "@/pages/RemindersPage";
 import useAuth from "./hooks/useAuth";
 import { LoginPage, RegisterPage } from "@/pages/Auth";
+import { lazy, Suspense } from "react";
+
+const AppLayout = lazy(() => import("@/layouts/AppLayout"));
 
 const App = () => {
   const { isAuth } = useAuth();
 
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={!isAuth ? <Navigate to="/login" /> : <AppLayout />}
-      >
-        <Route index element={<NotesPage />} />
-        <Route path="/reminders" element={<RemindersPage />} />
-      </Route>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route
+          path="/"
+          element={!isAuth ? <Navigate to="/login" /> : <AppLayout />}
+        >
+          <Route index element={<NotesPage />} />
+          <Route path="/reminders" element={<RemindersPage />} />
+        </Route>
 
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterPage />} />
-    </Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+    </Suspense>
   );
 };
 
