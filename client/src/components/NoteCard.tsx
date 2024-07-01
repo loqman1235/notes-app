08530f32706f6591c7789e60a2f7f8998f48b956
loqman1235 <tonyvito12@gmail.com>
2024-07-01 08:15:36 +0100

    commit

--- client/src/components/NoteCard.tsx
+++ client/src/components/NoteCard.tsx
@@ -4,7 +4,7 @@ import PinnButton from "./shared/PinnButton";
 const NoteCard = ({ title, content, bgColor, isPinned }: NoteCardProps) => {
   return (
     <div
-      className="rounded-md border p-5 hover:shadow-lg"
+      className="rounded-md border p-4 hover:shadow-lg"
       style={{
         backgroundColor: bgColor,
         borderColor:
@@ -16,13 +16,13 @@ const NoteCard = ({ title, content, bgColor, isPinned }: NoteCardProps) => {
       {/* TITLE */}
       {title && (
         <div className="mb-3 flex items-center justify-between">
-          <h2 className="text-xl font-bold">{title}</h2>
+          <h2 className="font-semibold">{title}</h2>
           <PinnButton isPinned={isPinned || false} />
         </div>
       )}
 
       {/* CONTENT */}
-      <p className="text-lg">{content}</p>
+      <p>{content}</p>
     </div>
   );
 };
